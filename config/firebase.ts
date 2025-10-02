9d5bf9732f54096f5706ebf3e383dd4a530290a9
jayasanki <ssunflower620@gmail.com>
2025-10-02 20:19:01 +0530

    update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDQof-b7AbRRvfDPm5Y6Ng9J-VmrhN962o",
   authDomain: "meditationtimerapp-4ade3.firebaseapp.com",
@@ -6,3 +12,18 @@ const firebaseConfig = {
   messagingSenderId: "41027114493",
   appId: "1:41027114493:web:46ded099dca13868e1b594"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
+
+// Export the app instance for use in other parts of your application
+export default app;
