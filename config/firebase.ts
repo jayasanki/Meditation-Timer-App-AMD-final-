979c6a3989d1f98da100ad46d81d49953229da49
jayasanki <ssunflower620@gmail.com>
2025-10-02 20:02:06 +0530

    Remove node_modules from repository and update .gitignore

---
+++ config/firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQof-b7AbRRvfDPm5Y6Ng9J-VmrhN962o",
+  authDomain: "meditationtimerapp-4ade3.firebaseapp.com",
+  projectId: "meditationtimerapp-4ade3",
+  storageBucket: "meditationtimerapp-4ade3.firebasestorage.app",
+  messagingSenderId: "41027114493",
+  appId: "1:41027114493:web:46ded099dca13868e1b594"
+};
