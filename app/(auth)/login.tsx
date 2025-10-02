d4642e1856be1eacc2b1405ac1df435241ba8388
jayasanki <ssunflower620@gmail.com>
2025-10-03 03:44:00 +0530

    export default function for loginScreen

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  ActivityIndicator
+} from 'react-native';
+import { Link, router } from 'expo-router';
+import { useAuth } from '@/context/AuthContext';
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const { login } = useAuth();
+
+  
+  };
+
+  return (
+  
+
+);
